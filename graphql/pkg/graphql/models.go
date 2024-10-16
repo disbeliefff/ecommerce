b0629fcde9439bf659718fa5ddacae289332fbfb
disbeliefff <sergeycrowler@gmail.com>
2024-10-16 22:01:52 +0300

    generated but not all works :)

---
+++ graphql/pkg/graphql/models.go
@@ -0,0 +1,10 @@
+// pkg/graphql/models.go
+package graphql
+
+import "time"
+
+type Account struct {
+	ID     string   `json:"id"`
+	Name   string   `json:"name"`
+	Orders []*Order `json:"orders"`
+}
