0384d29a31c93f4a17d154a8795336fdf8e568a2
disbeliefff <sergeycrowler@gmail.com>
2024-10-22 12:35:48 +0300

    [DB] Models, PostgreSQL config, psql utils

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -23,6 +23,13 @@ type Config struct {
 			Password string `env:"ADMIN_USER_PASSWORD" env-default:"dev"`
 		}
 	}
+	PostgreSQL struct {
+		Username string `env:"PSQL_USERNAME" env-required:"true"`
+		Password string `env:"PSQL_PASSWORD" env-required:"true"`
+		Host     string `env:"PSQL_HOST" env-required:"true"`
+		Port     string `env:"PSQL_PORT" env-required:"true"`
+		Database string `env:"PSQL_DATABASE" env-required:"true"`
+	}
 }
 
 var instance *Config
